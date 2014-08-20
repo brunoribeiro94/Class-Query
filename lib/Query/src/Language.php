8d42df8be16868951218d3ff9fa8b13858be56dd
Bruno Ribeiro <offboard@hotmail.com>
2014-08-20 14:51:18 -0300

    Create Language.php

---
+++ lib/Query/src/Language.php
@@ -0,0 +1,11 @@
+<?php
+
+class Language {
+
+    protected $PAGINATION_TEXT_DB_NAME = 'Unable to connect to database: ';
+    protected $PAGINATION_TEXT_ERRO_QUERY = 'Error: bad query type: ';
+    protected $PAGINATION_TEXT_ERRO_TYPE_QUERY = 'Error in query: ';
+    protected $PAGINATION_TEXT_BEFORE = 'Before';
+    protected $PAGINATION_TEXT_AFTER = 'After';
+
+}
