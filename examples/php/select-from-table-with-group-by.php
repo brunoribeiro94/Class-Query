3efcc7abc4a793acfd7cab4fd03ea5db7abbc49e
paulowww <henrique_s3enoiss3@hotmail.com>
2014-08-20 15:33:56 -0300

    Update select-from-table-with-group-by.php
    fix path javascript

--- examples/php/select-from-table-with-group-by.php
+++ examples/php/select-from-table-with-group-by.php
@@ -94,9 +94,9 @@ if (!($result && $count > 0)) {
         ORDER BY
                 `name` ASC
 */</pre>
-            <script src="../../examples/js/prettify/prettify_core_compiled.js"></script>
+            <script src="../js/prettify/prettify_core_compiled.js"></script>
             <script type="text/javascript">prettyPrint();</script>
-            <script type="text/javascript" src="../../examples/js/ph_core.js"></script>
+            <script type="text/javascript" src="../js/ph_core.js"></script>
         </div>
     </body>
 </html>
