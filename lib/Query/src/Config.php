11a6e985f4a30e321ae26b12853ef4fcf520a32f
Bruno Ribeiro <offboard@hotmail.com>
2014-08-24 19:55:24 -0300

    Update Config.php

--- lib/Query/src/Config.php
+++ lib/Query/src/Config.php
@@ -3,12 +3,18 @@
 /**
  * Configuration for: Database Connection
  * This is the place where your database constants are saved
+ * @version 2.3
  *
  */
 class Config extends Run {
 
     /**
      * Multiple Database Conection
+     * DB_HOST - database host, usually it's "127.0.0.1" or "localhost", some servers also need port info
+     * DB_NAME - for set name of the database. please note: database and database table are not the same thing
+     * DB_USER - for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT.
+     * by the way, it's bad style to use "root", but for development it will work.
+     * DB_PASS - the password of the above user
      * @var Array 
      */
     public $Conections_Settings = array(
@@ -17,32 +23,27 @@ class Config extends Run {
             'DB_NAME' => 'class_Query',
             'DB_USER' => 'root',
             'DB_PASS' => '',
-            'DB_CHARSET' => 'utf8'
+            'DB_CHARSET' => 'utf8',
+            'DB_Status' => false
         ),
         'Data2' => array(
             'DB_HOST' => 'localhost',
             'DB_NAME' => 'class_Query_dev',
             'DB_USER' => 'root',
             'DB_PASS' => '',
-            'DB_CHARSET' => 'utf8'
+            'DB_CHARSET' => 'utf8',
+            'DB_Status' => false
         )
     );
 
     /**
      * Link mysqli please no put nothing here
-     * @var array 
-    */
+     * @var Array 
+     */
     protected $link_mysqi = array();
-    
-    /**
-     * set charset
-     * @deprecated since version 2.2
-     * @var string 
-    */
-    protected $charset = 'UTF8';
 
     /**
-     * Make Conection Mysqi (recommended)
+     * Make a conection or multiples conections Mysqi (recommended)
      * @access Private
      * @return Void
      */
@@ -50,11 +51,11 @@ class Config extends Run {
         foreach ($this->Conections_Settings as $key => $value) {
             try {
                 $mysqli = new mysqli($value['DB_HOST'], $value['DB_USER'], $value['DB_PASS'], $value['DB_NAME']);
+                $mysqli->set_charset($value['DB_CHARSET']);
+                $this->link_mysqi[] = $mysqli;
             } catch (Exception $e) {
                 exit($this->TEXT_DB_NAME . $e->message);
             }
-            $mysqli->set_charset($this->charset);
-            $this->link_mysqi[$key] = $mysqli;
         }
     }
 
