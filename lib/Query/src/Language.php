498f40e776aa1573a2af0b20a313c544cfd47493
Kronoax <swt-rikeesantos@hotmail.com>
2014-09-11 18:27:55 -0300

    Update Language.php

--- lib/Query/src/Language.php
+++ lib/Query/src/Language.php
@@ -1,11 +1,9 @@
 <?php
 
 class Language {
-
-    protected $TEXT_DB_NAME = 'Unable to connect to database: ';
-    protected $TEXT_ERRO_QUERY = 'Error: bad query type: ';
-    protected $TEXT_ERRO_TYPE_QUERY = 'Error in query: ';
-    protected $PAGINATION_TEXT_BEFORE = 'Before';
-    protected $PAGINATION_TEXT_AFTER = 'After';
-
+    var $TEXT_DB_NAME = 'Unable to connect to database: ';
+    var $TEXT_ERRO_QUERY = 'Error: bad query type: ';
+    var $TEXT_ERRO_TYPE_QUERY = 'Error in query: ';
+    var $PAGINATION_TEXT_BEFORE = 'Before';
+    var $PAGINATION_TEXT_AFTER = 'After';
 }
