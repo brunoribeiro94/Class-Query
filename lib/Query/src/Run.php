b086ea782c0362f49b52f083a45eb8bb6c53cbe8
Bruno Ribeiro <offboard@hotmail.com>
2014-10-03 20:31:34 -0300

    Update Run.php
    GNU style formatter

--- lib/Query/src/Run.php
+++ lib/Query/src/Run.php
@@ -1,16 +1,20 @@
 <?php
 
-class Run extends Get {
-
+class Run extends Get
+  {
+    
     /**
      * Execute Query
      * runs query, returns mysql result
      * @return \Query
      */
-    public function run() {
-        if (self::get()) {
+    public function run()
+      {
+        if (self::get())
+          {
             $function = '_run_' . $this->query_type;
-            switch ($this->query_type) {
+            switch ($this->query_type)
+            {
                 case 'delete':
                 case 'insert_ignore_into':
                 case 'insert_into':
@@ -20,80 +24,98 @@ class Run extends Get {
                     return self::$function();
                     break;
                 case 'select':
-                    if (!(isset($this->page) || isset($this->offset))) {
+                    if (!(isset($this->page) || isset($this->offset)))
+                      {
                         // no pagination
                         return self::$function();
-                    } else {
+                      }
+                    else
+                      {
                         // with pagination
-                        if (self::$function()) {
+                        if (self::$function())
+                          {
                             // for pagination:
                             $this->perpage = $this->limit; // for get_perpage()
-                            $this->total = $this->results; // for get_total()
+                            $this->total   = $this->results; // for get_total()
                             // calculate pages
-                            $this->pages = (int) ceil($this->results / $this->limit);
+                            $this->pages   = (int) ceil($this->results / $this->limit);
                             // set offset
-                            if (!isset($this->offset)) {
+                            if (!isset($this->offset))
+                              {
                                 $this->offset(($this->page * $this->limit) - $this->limit);
-                            } else {
+                              }
+                            else
+                              {
                                 // calculate page using offset and perpage
                                 // determine on what page the offset would be on
-                                for ($page = 1; $page <= $this->pages; $page++) {
-                                    if ($this->offset - 1 < $page * $this->perpage) {
+                                for ($page = 1; $page <= $this->pages; $page++)
+                                  {
+                                    if ($this->offset - 1 < $page * $this->perpage)
+                                      {
                                         $this->page = $page;
                                         break;
-                                    }
-                                }
-                            }
+                                      }
+                                  }
+                              }
                             // update select query with limit now that pages is set
                             self::_get_select_query(true);
                             // run select query with updated limit and offset
                             return self::_run_select();
-                        }
-                    }
+                          }
+                      }
                     break;
                 default:
                     die($this->TEXT_ERRO_QUERY . $this->query_type);
                     break;
             }
-        }
+          }
         return false;
-    }
-
-    private function _run_delete() {
+      }
+    
+    private function _run_delete()
+      {
         return self::_run_query($this->delete_query);
-    }
-
-    private function _run_insert_ignore_into() {
+      }
+    
+    private function _run_insert_ignore_into()
+      {
         return self::_run_query($this->insert_query);
-    }
-
-    private function _run_insert_into() {
+      }
+    
+    private function _run_insert_into()
+      {
         return self::_run_query($this->insert_query);
-    }
-
-    private function _run_insert_multiple() {
+      }
+    
+    private function _run_insert_multiple()
+      {
         return self::_run_query($this->insert_multiple_query);
-    }
-
-    private function _run_replace_into() {
+      }
+    
+    private function _run_replace_into()
+      {
         return self::_run_query($this->replace_into);
-    }
-
-    private function _run_select() {
+      }
+    
+    private function _run_select()
+      {
         return self::_run_query($this->select_query);
-    }
-
-    private function _run_update() {
+      }
+    
+    private function _run_update()
+      {
         return self::_run_query($this->update_query);
-    }
-
+      }
+    
     /**
      * checks what action was called
      * @param Unknow $param query type
      * @return Object
      */
-    private function _run_query_query_type($param) {
-        switch ($param) {
+    private function _run_query_query_type($param)
+      {
+        switch ($param)
+        {
             case 'delete':
                 return self::get_affected();
             case 'insert_into':
@@ -104,45 +126,54 @@ class Run extends Get {
                 return self::get_affected();
             case 'select':
                 self::_get_results();
-                if ($this->result && $this->results > 0) {
+                if ($this->result && $this->results > 0)
+                  {
                     return $this->result;
-                }
+                  }
                 return false;
             case 'update':
                 return self::get_affected();
         }
-    }
-
+      }
+    
     /**
      * Run Query
      * @param type $query
      * @version 1.3
      * @return Object
      */
-    private function _run_query($query) {
-        if (count($this->Connections_Settings) == 1) {
+    private function _run_query($query)
+      {
+        if (count($this->Connections_Settings) == 1)
+          {
             $result = mysqli_query($this->link_mysqi[0], $query);
             $mysqli = $this->link_mysqi[0];
-        } else {
-            for ($i = 0; $i < count($this->Connections_Settings); $i++) {
+          }
+        else
+          {
+            for ($i = 0; $i < count($this->Connections_Settings); $i++)
+              {
                 $link = $this->link_mysqi[$i];
-                if (mysqli_query($link, $query)) {
+                if (mysqli_query($link, $query))
+                  {
                     $result = mysqli_query($link, $query);
                     $mysqli = $link;
-                }
-            }
-        }
+                  }
+              }
+          }
         $this->result = $result;
-        if (!$this->result) {
+        if (!$this->result)
+          {
             $this->mysql_error = mysqli_error($mysqli);
-            $this->error = $this->TEXT_ERRO_TYPE_QUERY . $this->mysql_error;
-
-            if (function_exists('error')) {
+            $this->error       = $this->TEXT_ERRO_TYPE_QUERY . $this->mysql_error;
+            
+            if (function_exists('error'))
+              {
                 error($this->error);
-            }
+              }
             die($this->error);
-        }
+          }
         return $this->_run_query_query_type($this->query_type);
-    }
-
-}
+      }
+    
+  }
