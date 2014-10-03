e98f7da59cef848ad3e746de939a1d8eb3099151
Bruno Ribeiro <offboard@hotmail.com>
2014-10-03 20:32:03 -0300

    Update Update.php
    GNU style formatter

--- lib/Query/src/Update.php
+++ lib/Query/src/Update.php
@@ -1,38 +1,43 @@
 <?php
 
-class Update extends Where {
+class Update extends Where
+  {
     /* UPDATE */
-
+    
     /**
      * return last get updated
      * @return array
      */
-    public function get_updated() {
+    public function get_updated()
+      {
         return self::get_affected();
-    }
-
+      }
+    
     /**
      * values set for update Query function
      * @param mixed $set
      * @return \Update
      */
-    public function set($set) {
+    public function set($set)
+      {
         $this->set = $set;
         return $this;
-    }
-
+      }
+    
     /**
      * Function Query update
      * @param string $update
      * @param array $set
      * @return \Update
      */
-    public function update($update, $set = array()) {
+    public function update($update, $set = array())
+      {
         $this->update = $update;
-        if (!empty($set)) {
+        if (!empty($set))
+          {
             self::set($set);
-        }
+          }
         return $this;
-    }
-
-}
+      }
+    
+  }
