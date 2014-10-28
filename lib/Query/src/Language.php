7ce2bc34380b75002c333a7bf1ec8248bd31f725
Bruno Ribeiro <offboard@hotmail.com>
2014-10-28 15:14:21 -0300

    Update Language.php
    improvement in documentation

--- lib/Query/src/Language.php
+++ lib/Query/src/Language.php
@@ -1,9 +1,17 @@
 <?php
 
+/**
+ * Text used in class Query
+ * @author Bruno Ribeiro <bruno.espertinho@gmail.com>
+ * 
+ * @version 0.1
+ * @access public
+ * @package Delete
+ */
 class Language {
-    var $TEXT_DB_NAME = 'Unable to connect to database: ';
-    var $TEXT_ERRO_QUERY = 'Error: bad query type: ';
-    var $TEXT_ERRO_TYPE_QUERY = 'Error in query: ';
-    var $PAGINATION_TEXT_BEFORE = 'Before';
-    var $PAGINATION_TEXT_AFTER = 'After';
+    public $TEXT_DB_NAME = 'Unable to connect to database: ';
+    public $TEXT_ERRO_QUERY = 'Error: bad query type: ';
+    public $TEXT_ERRO_TYPE_QUERY = 'Error in query: ';
+    public $PAGINATION_TEXT_BEFORE = 'Before';
+    public $PAGINATION_TEXT_AFTER = 'After';
 }
