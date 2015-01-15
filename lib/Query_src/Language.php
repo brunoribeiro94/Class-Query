c5c45bd08c89a5662e3c191246515619361079d5
Bruno Ribeiro <offboard@users.noreply.github.com>
2015-01-15 18:20:40 -0300

    Update Language.php

--- lib/Query_src/Language.php
+++ lib/Query_src/Language.php
@@ -1,21 +1,71 @@
 <?php
+
 //namespace to organize
+
 namespace Query_src;
 
 /**
  * Text used in class Query
  * @author Bruno Ribeiro <bruno.espertinho@gmail.com>
  * 
- * @version 0.1
+ * @version 0.2
  * @access public
- * @package Delete
+ * @package text_class
  */
 class Language {
 
-    public $TEXT_DB_NAME = 'Unable to connect to database: ';
-    public $TEXT_ERRO_QUERY = 'Error: bad query type: ';
-    public $TEXT_ERRO_TYPE_QUERY = "Query error in table '%s' mysql error : %s";
-    public $PAGINATION_TEXT_BEFORE = 'Before';
-    public $PAGINATION_TEXT_AFTER = 'After';
+    /**
+     * Error connect database
+     * 
+     * @static
+     * @access protected
+     * @var String
+     */
+    protected static $TEXT_DB_NAME = 'Unable to connect to database: ';
+
+    /**
+     * Error invalid execute Query
+     * 
+     * @static
+     * @access protected
+     * @var String
+     */
+    protected static $TEXT_ERRO_QUERY = 'Error: bad query type: ';
+
+    /**
+     * Error query syntax
+     * 
+     * @static
+     * @access protected
+     * @var String
+     */
+    protected static $TEXT_ERRO_TYPE_QUERY = 'Error in query: ';
+
+    /**
+     * Text debugger  to show SQL executed
+     * 
+     * @static
+     * @access protected
+     * @var String
+     */
+    protected static $TEXT_OUTPUT_QUERY = 'SQL executed: ';
+
+    /**
+     * Text pagination before page
+     * 
+     * @static
+     * @access protected
+     * @var String
+     */
+    protected static $PAGINATION_TEXT_BEFORE = 'Before';
+
+    /**
+     * Text pagination after page
+     * 
+     * @static
+     * @access protected
+     * @var String
+     */
+    protected static $PAGINATION_TEXT_AFTER = 'After';
 
 }
